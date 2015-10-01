273995c3ee7a0d29169fbe24fc287bce723a832d
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2015-10-02 01:00:05 +0530

    eu3.py

--- euler/3/eu3.py
+++ euler/3/eu3.py
@@ -1,24 +1,13 @@
-import math
-list=[2]
-
-def primelist(num):
-    count = 3
-    
-    while count<num:
-        isprime = True
-        
-        for x in range(2, int(math.sqrt(count) + 1)):
-            if count % x == 0: 
-                isprime = False
-                break
-        
-        if isprime:
-            list.append(count)
-        
-        count += 2
-
-
-
-num=int(raw_input())
-primelist(num)
-print list
+T=int(raw_input())
+while(T>0):
+	num=int(raw_input())
+	largfact=1
+	i=2
+	while(i*i <= num):
+		if(num%i==0):
+			num=num/i
+			largfact=i
+		else:
+			i+=1
+	print max(largfact,num)
+	T-=1
